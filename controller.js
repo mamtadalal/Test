55292d299d2c66a2e017e42febe21d1f56d0d7b3
mamtadalal <dmamta8@gmail.com>
2016-03-09 17:02:45 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,4 +1,35 @@
 console.log("1");
+angular.module("Demo", []).controller("myCtrl", function($scope) {
+    $scope.people = [
+    {
+      name: 'John Doe',
+      city: 'New York City',
+      state: 'New York'
+    },
+    {
+      name: 'John Smith',
+      city: 'Oklahoma City',
+      state: 'Oklahoma'
+    },
+    {
+      name: 'Henry Black',
+      city: 'Topeka',
+      state: 'Kansas'
+    }
+  ];
+  $scope.addPerson = function () {
+    $scope.people.push({
+        name: $scope.person_name,
+        city: $scope.person_city,
+        state: $scope.person_state
+    });
+    $scope.person_name = '';
+    $scope.person_city = '';
+    $scope.person_state = '';
+  }
+});
+
+/*
 var App = angular.module('Test', []);
 console.log("2");
 App.controller('MainCtrl', function ($scope) {
@@ -32,3 +63,4 @@ App.controller('MainCtrl', function ($scope) {
     $scope.person_state = '';
   }
 }
+*/
